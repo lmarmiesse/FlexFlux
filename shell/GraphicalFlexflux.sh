16b4e4504bd75533074d6413cffbd228dbd71b32
lcottret <ludovic.cottret@toulouse.inra.fr>
2014-08-28 14:10:00 +0200

    Debug ConditoionComparison and shell config

--- shell/GraphicalFlexflux.sh
+++ shell/GraphicalFlexflux.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source config
 
-eval "java -Djava.library.path=$CPLEX_shared_library:$GLPK_shared_library -cp bin/:lib/*:$CPLEX_JAR:$GLPK_JAR flexflux.applications.gui.GraphicalFlexflux"
+source $path/config
+
+cmd="java -Djava.library.path=$CPLEX_shared_library:$GLPK_shared_library -cp $GLPK_JAR:$path/lib/flexflux.jar flexflux.applications.gui.GraphicalFlexflux"
+
+echo $cmd
+
+eval $cmd
