c9cc557225b7a1fe13c1770ff8dd954e5dedd927
lmarmiesse <elbeaudu33@gmail.com>
2015-11-20 09:34:03 +0000

    new jar and new zip

---
+++ src/flexflux/omics/TestFreeFluxes.java
@@ -0,0 +1,405 @@
+package flexflux.omics;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import flexflux.analyses.FBAAnalysis;
+import flexflux.general.Bind;
+import flexflux.general.Constraint;
+import flexflux.general.CplexBind;
+import flexflux.general.GLPKBind;
+import flexflux.general.Vars;
+import flexflux.interaction.Interaction;
+import flexflux.objective.Objective;
+import parsebionet.biodata.BioChemicalReaction;
+import parsebionet.biodata.BioEntity;
+
+public class TestFreeFluxes {
+
+	public static void main(String[] args) {
+
+		/**
+		 * 1 => And : sum ; or : mean <br/>
+		 * 2 => all mean 3 => And : sum ; or : min <br/>
+		 */
+		int gprCalculationMethod = 2;
+
+		Map<String, Map<String, Double>> revResults = new HashMap<String, Map<String, Double>>();
+		Map<String, Map<String, Double>> fbaResults = new HashMap<String, Map<String, Double>>();
+
+//		String metabolicNetworkPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/AT/AraGEM_Cobra_modif.xml";
+//		String omicsDataPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/AT/MYBSEQ_TIME.txt";
+//		String revResultsPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/AT/resultsAT_time_rev.txt";
+//		String fbaResultsPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/AT/resultsAT_time_fba.txt";
+		
+//		String omicsDataPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/AT/MYBSEQ_GTYPE_tps0.txt";
+//		String revResultsPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/AT/resultsAT_gtype_rev.txt";
+//		String fbaResultsPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/AT/resultsAT_gtype_fba.txt";
+		
+		
+//		String metabolicNetworkPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/Toy model/toyModel.xml";
+//		String omicsDataPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/Toy model/exprData.txt";
+//		String revResultsPath ="/home/lmarmiesse/rocks1-compneur/work/lucas/Toy model/resultsRev.txt";
+//		String fbaResultsPath ="/home/lmarmiesse/rocks1-compneur/work/lucas/Toy model/resultsFBA.txt";
+		String metabolicNetworkPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/Toy model/toyModel2.xml";
+		String omicsDataPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/Toy model/exprData2.txt";
+		String revResultsPath ="/home/lmarmiesse/rocks1-compneur/work/lucas/Toy model/resultsRev2.txt";
+		String fbaResultsPath ="/home/lmarmiesse/rocks1-compneur/work/lucas/Toy model/resultsFBA2.txt";		
+		
+		
+		
+		
+		
+		
+		//RECON 2
+//		String metabolicNetworkPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/recon2/Recon2.v02_with_anno-MODEL1109130000.xml";
+//		String omicsDataPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/recon2/test.txt";
+//		String revResultsPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/recon2/results_recon2_rev.txt";
+//		String fbaResultsPath = "/home/lmarmiesse/rocks1-compneur/work/lucas/recon2/results_recon2_fba.txt";
+		
+		
+		
+		
+
+//		 Bind bind = new GLPKBind();
+		Bind bind = new CplexBind();
+
+		bind.loadSbmlNetwork(metabolicNetworkPath, false);
+
+		// need to "free" every flux
+		for (BioEntity bioEntity : bind.getInteractionNetwork().getEntities()) {
+			if (!bind.getDeadReactions().contains(bioEntity)) {
+				Constraint toDelete = null;
+				for (Constraint c : bind.getConstraints()) {
+					
+					Map<BioEntity, Double> entities = c.getEntities();
+
+					if (entities.size() == 1 && entities.containsKey(bioEntity)) {
+						
+						if (entities.get(bioEntity) == 1) {
+							
+							toDelete = c;
+							
+//							if (c.getLb()<0)
+//							{
+//								System.out.println(c);
+//							}
+//							c.setLb(-100);
+//							c.setUb(100);
+//							
+							
+							break;
+						}
+					}
+				}
+				if (toDelete != null) {
+					
+					// System.out.println(toDelete);
+					
+					bind.getConstraints().remove(toDelete);
+					bind.getSimpleConstraints().remove(bioEntity);
+					
+					
+					double lb = -70000;
+					double ub = 70000;
+
+					Map<BioEntity, Double> constraintMap = new HashMap<BioEntity, Double>();
+					constraintMap.put(bioEntity, 1.0);
+					
+					if (!((BioChemicalReaction) bioEntity).isReversible()){
+						lb=0;
+					}
+					
+					Constraint c = new Constraint(constraintMap, lb, ub);
+					
+					bind.getConstraints().add(c);
+					bind.getSimpleConstraints().put(bioEntity, c);
+					
+				}
+			}
+		}
+		
+
+		OmicsData omicsData = OmicsDataReader.loadOmicsData(omicsDataPath, bind.getInteractionNetwork().getEntities());
+
+		List<Sample> samples = omicsData.getSamples();
+
+		Map<Sample, Map<BioChemicalReaction, Double>> reactionExpressionValues = new HashMap<Sample, Map<BioChemicalReaction, Double>>();
+
+		for (Sample s : samples) {
+			reactionExpressionValues.put(s, new HashMap<BioChemicalReaction, Double>());
+		}
+
+		
+		
+		for (Sample s : samples) {
+			revResults.put(s.getName(), new HashMap<String, Double>());
+
+			for (Interaction inter : bind.getInteractionNetwork().getGPRInteractions()) {
+				BioChemicalReaction reac = (BioChemicalReaction) inter.getConsequence().getEntity();
+
+				double expr = inter.getCondition()
+						.calculateRelationQuantitativeValue(omicsData.getDataValuesForSample(s), gprCalculationMethod);
+
+				if (!Double.isNaN(expr)) {
+					
+					expr/=100;
+					
+					revResults.get(s.getName()).put(reac.getId(), expr);
+					reactionExpressionValues.get(s).put(reac, expr);
+				}
+			}
+		}
+		
+		writeFbaResults(revResultsPath, revResults,samples);
+
+		BioEntity fluxSum = bind.createFluxesSummation();
+
+		for (Sample sample : samples) {
+			
+			System.out.println(sample.getName());
+
+			fbaResults.put(sample.getName(), new HashMap<String, Double>());
+
+			///////////// We create FBA variables that correspond to the
+			///////////// distance
+			///////////// between scaled reaction expression values and flux
+			///////////// values.
+			///////////// We also create the objective function that minimises
+			///////////// the
+			///////////// distances
+
+			Map<BioChemicalReaction, Double> reactionExpressionValues_sample = reactionExpressionValues.get(sample);
+
+			BioEntity[] objectiveEntities = new BioEntity[reactionExpressionValues_sample.size()];
+			double[] objectiveCoeffs = new double[reactionExpressionValues_sample.size()];
+			int index = 0;
+
+			List<Constraint> constraintsToAdd = new ArrayList<Constraint>();
+
+			for (BioChemicalReaction reac : reactionExpressionValues_sample.keySet()) {
+
+				BioEntity reacPrime = new BioEntity(reac.getId() + "_prime");
+				bind.getInteractionNetwork().addNumEntity(reacPrime);
+				objectiveEntities[index] = reacPrime;
+				objectiveCoeffs[index] = 1.0;
+
+				Constraint c1, c2;
+				
+				if (bind.getSimpleConstraints().get(reac).getLb() >= 0) {
+					
+					// we create the constraints : R1 - R1exp <= R'1
+					// R1exp - R1 <= R'1
+
+					// Const 1 : -inf < R1 - R1exp - R'1 <= 0
+					// <=> -inf < R1 - R'1 <= R1exp
+					Map<BioEntity, Double> cMap1 = new HashMap<BioEntity, Double>();
+					cMap1.put(reac, 1.0);
+					cMap1.put(reacPrime, -1.0);
+					c1 = new Constraint(cMap1, -Double.MAX_VALUE, reactionExpressionValues_sample.get(reac));
+					// System.out.println(c1);
+
+					// Const 2 : -inf < R1exp - R1 - R'1 <= 0
+					// <=> -inf < -R1 - R'1 <= - R1exp
+					Map<BioEntity, Double> cMap2 = new HashMap<BioEntity, Double>();
+					cMap2.put(reac, -1.0);
+					cMap2.put(reacPrime, -1.0);
+					c2 = new Constraint(cMap2, -Double.MAX_VALUE, -reactionExpressionValues_sample.get(reac));
+					// System.out.println(c2);
+
+				}
+				// reversible reactions
+				else {
+					BioEntity irrevReac1 = bind.getInteractionNetwork().getEntity(reac.getId() + Vars.Irrev1);
+					BioEntity irrevReac2 = bind.getInteractionNetwork().getEntity(reac.getId() + Vars.Irrev2);
+
+					// we create the constraints : (R1irrev1-R1irrev2) - R1exp
+					// <=
+					// R'1
+					// R1exp - (R1irrev1-R1irrev2) <= R'1
+
+					// Const 1 : -inf < R1irrev1-R1irrev2 - R1exp - R'1 <= 0
+					// <=> -inf < R1irrev1-R1irrev2 - R'1 <= R1exp
+					Map<BioEntity, Double> cMap1 = new HashMap<BioEntity, Double>();
+					cMap1.put(irrevReac1, 1.0);
+					cMap1.put(irrevReac2, 1.0);
+					cMap1.put(reacPrime, -1.0);
+					c1 = new Constraint(cMap1, -Double.MAX_VALUE, reactionExpressionValues_sample.get(reac));
+					// System.out.println(c1);
+
+					// Const 2 : -inf < R1exp - (R1irrev1-R1irrev2) - R'1 <= 0
+					// <=> -inf < -(R1irrev1-R1irrev2) - R'1 <= - R1exp
+					// <=> -inf < - R1irrev1 + R1irrev2 - R'1 <= - R1exp
+					Map<BioEntity, Double> cMap2 = new HashMap<BioEntity, Double>();
+					cMap2.put(irrevReac1, -1.0);
+					cMap2.put(irrevReac2, -1.0);
+					cMap2.put(reacPrime, -1.0);
+					c2 = new Constraint(cMap2, -Double.MAX_VALUE, -reactionExpressionValues_sample.get(reac));
+
+					/////////////////// Then we make sure that the summ of the 2
+					/////////////////// components
+					/////////////////// of a reversible reaction is = to the
+					/////////////////// absolute flux
+					/////////////////// values of the reaction (not more)
+
+					///////////// Need to add two boolean variables : b and a =
+					///////////// 1-b
+					BioEntity b = new BioEntity("interger_mip_b_" + reac.getId());
+					bind.getInteractionNetwork().addBinaryEntity(b);
+					BioEntity a = new BioEntity("interger_mip_a_" + reac.getId());
+					bind.getInteractionNetwork().addBinaryEntity(a);
+
+					/// constraint a = 1-b <=> a-1+b = 0 <=> a+b=1
+					Map<BioEntity, Double> intergerConstraintMap = new HashMap<BioEntity, Double>();
+					intergerConstraintMap.put(a, 1.0);
+					intergerConstraintMap.put(b, 1.0);
+					Constraint integerConstraint = new Constraint(intergerConstraintMap, 1.0, 1.0);
+					constraintsToAdd.add(integerConstraint);
+
+					////////////// We add an entity Y that is R1a + R1b
+					////////////// <=> Y-Raa-R1b = 0
+					BioEntity y = new BioEntity("Y_" + reac.getId());
+					bind.getInteractionNetwork().addNumEntity(y);
+					Map<BioEntity, Double> yConstraintMap = new HashMap<BioEntity, Double>();
+					yConstraintMap.put(y, 1.0);
+					yConstraintMap.put(irrevReac1, -1.0);
+					yConstraintMap.put(irrevReac2, -1.0);
+					Constraint yConstraint = new Constraint(yConstraintMap, 0.0, 0.0);
+					constraintsToAdd.add(yConstraint);
+
+					////////////// http://lpsolve.sourceforge.net/5.1/absolute.htm
+					////////////// We add these two constrains:
+					// R1 + M * B >= Y
+					// -R1 + M * A >= Y
+					// <=>
+					// R1 + M*B - Y >= 0
+					// -R1 + M*A - Y >= 0
+
+					double M = 100;
+					// first const
+					Map<BioEntity, Double> intergerSumConstraintMap1 = new HashMap<BioEntity, Double>();
+					intergerSumConstraintMap1.put(reac, 1.0);
+					intergerSumConstraintMap1.put(b, M);
+					intergerSumConstraintMap1.put(y, -1.0);
+
+					Constraint integerSumConstraint1 = new Constraint(intergerSumConstraintMap1, 0.0, Double.MAX_VALUE);
+
+					constraintsToAdd.add(integerSumConstraint1);
+
+					// second const
+					Map<BioEntity, Double> intergerSumConstraintMap2 = new HashMap<BioEntity, Double>();
+					intergerSumConstraintMap2.put(reac, -1.0);
+					intergerSumConstraintMap2.put(a, M);
+					intergerSumConstraintMap2.put(y, -1.0);
+					Constraint integerSumConstraint2 = new Constraint(intergerSumConstraintMap2, 0.0, Double.MAX_VALUE);
+
+					constraintsToAdd.add(integerSumConstraint2);
+
+					// System.out.println(c2);
+				}
+				constraintsToAdd.add(c1);
+				constraintsToAdd.add(c2);
+				index++;
+			}
+
+			bind.prepareSolver();
+
+			Objective p = new Objective(objectiveEntities, objectiveCoeffs, "", false);
+			bind.setObjective(p);
+
+			double resFBA = bind.FBA(constraintsToAdd, true, false).result;
+
+			System.out.println(resFBA);
+			
+			// write results
+			for (String reacName : bind.getBioNetwork().getBiochemicalReactionList().keySet()) {
+				BioChemicalReaction reac = bind.getBioNetwork().getBiochemicalReactionList().get(reacName);
+				fbaResults.get(sample.getName()).put(reacName, bind.getSolvedValue(reac));
+			}
+
+		}
+
+		writeFbaResults(fbaResultsPath, fbaResults,samples);
+	}
+
+	public static void writeFbaResults(String resultsPath, Map<String, Map<String, Double>> fbaResults,List<Sample> samples) {
+
+		try {
+			PrintWriter out = new PrintWriter(new File(resultsPath));
+
+			String firstLine = "";
+
+			List<String> sampleNames = new ArrayList<>();
+			List<String> reacNames = new ArrayList<>();
+
+			for (Sample sample: samples) {
+				sampleNames.add(sample.getName());
+				firstLine += sample.getName() + "\t";
+			}
+
+			for (String reacName : fbaResults.get(sampleNames.get(0)).keySet()) {
+				reacNames.add(reacName);
+			}
+
+			firstLine = firstLine.substring(0, firstLine.length() - 1);
+			out.println(firstLine);
+
+			for (String reacName : reacNames) {
+				String line = reacName;
+				for (String sampleName : sampleNames) {
+					line += "\t" + fbaResults.get(sampleName).get(reacName);
+				}
+				out.println(line);
+			}
+
+			out.close();
+		} catch (IOException e) {
+			System.out.println("path " + resultsPath + " is not a valid path, or file could not be created.");
+		}
+
+	}
+
+	public static void writeRevResults(String resultsPath, Map<String, Map<String, Double>> revResults,List<Sample> samples) {
+
+		try {
+			PrintWriter out = new PrintWriter(new File(resultsPath));
+
+			String firstLine = "";
+
+			List<String> sampleNames = new ArrayList<>();
+			List<String> reacNames = new ArrayList<>();
+
+			for (Sample sample: samples) {
+				sampleNames.add(sample.getName());
+				firstLine += sample.getName() + "\t";
+			}
+
+			for (String reacName : revResults.get(sampleNames.get(0)).keySet()) {
+				reacNames.add(reacName);
+			}
+
+			firstLine = firstLine.substring(0, firstLine.length() - 1);
+			out.println(firstLine);
+
+			for (String reacName : reacNames) {
+				String line = reacName;
+				for (String sampleName : sampleNames) {
+					line += "\t" + revResults.get(sampleName).get(reacName);
+				}
+				out.println(line);
+			}
+
+			out.close();
+		} catch (IOException e) {
+			System.out.println("path " + resultsPath + " is not a valid path, or file could not be created.");
+		}
+
+	}
+
+}
